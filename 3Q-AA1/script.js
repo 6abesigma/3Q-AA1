182163d69cf59fee97e5ae2e4573962e716636e9
6abesigma <cgcdavis22@src.pshs.edu.ph>
2025-01-24 01:41:07 -0800

    script.js

--- 3Q-AA1/script.js
+++ 3Q-AA1/script.js
@@ -4,7 +4,7 @@ let circumference = 2 * pi * radius;
 let surfacearea = 4 * pi * radius ** 2;
 let volume = (4/3) * pi * radius ** 3;
 
-document.write('The Radius is' + radius <br>);
-document.write('The circumference is' + circumference <br>);
-document.write('The surfacearea is' + surfacearea <br>);
-document.write('The volume is' + volume <br>);
+document.write("The Radius is" + radius + "<br>");
+document.write("The circumference is" + circumference + "<br>");
+document.write("The surfacearea is" + surfacearea + "<br>");
+document.write("The volume is" + volume + "<br>");
