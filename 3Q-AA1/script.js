a2e6a0d0a0f51e2beda929342a0e59e18788c7e6
6abesigma <cgcdavis22@src.pshs.edu.ph>
2025-01-24 08:01:09 +0800

    script.js

--- 3Q-AA1/script.js
+++ 3Q-AA1/script.js
@@ -1,5 +1,10 @@
-const pi = 3.14
+const pi = 3.14;
 let radius;
-let circumference = 2 * pi * radius
-let surfacearea = 4 * pi * radius ** 2
-let volume = (4/3) * pi * radius ** 3
+let circumference = 2 * pi * radius;
+let surfacearea = 4 * pi * radius ** 2;
+let volume = (4/3) * pi * radius ** 3;
+
+document.write('The Radius is' + radius <br>);
+document.write('The circumference is' + circumference <br>);
+document.write('The surfacearea is' + surfacearea <br>);
+document.write('The volume is' + volume <br>);
